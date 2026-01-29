e2e4f58fcfcdb5eafc6c8ba0d30f8936fa921757
Gary Fan <garyfan12@gmail.com>
2026-01-29 16:08:49 +0800

    initial commit

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+import json
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def health_check(request):
+    return Response({"msg": "pong"})

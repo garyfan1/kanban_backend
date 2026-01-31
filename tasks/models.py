c3d0186b7b3f176f95688f147a9b4416b440e0e3
Gary Fan <garyfan12@gmail.com>
2026-02-01 06:55:54 +0800

    feat: added Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=50)
+    description = models.TextField(default="", null=False, blank=True)
+    status = models.CharField(choices=[("todo", "To Do"), ("doing", "In Progress"), ("done", "Done")], default="todo", max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"

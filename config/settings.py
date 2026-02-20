85ce0cb66414592d0095f380f9c9d7be0864689f
Gary Fan <garyfan12@gmail.com>
2026-02-21 04:37:29 +0800

    fix: add allowed origin for nuxt

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,9 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 SIMPLE_JWT = {
